7ad667412be02deb57727a1254f4414d96bf24df
qinhant <qinhant@princeton.edu>
2024-03-27 14:26:38 -0400

    add an assumption to verify.tcl

--- verify.tcl
+++ verify.tcl
@@ -5,10 +5,13 @@ elaborate -top fifo
 clock clk
 reset rst
 
-#assertion to check fifo will not overflow (##1 means after 1 cycle, $stable is true if the signal's value is the same as last cycle)
+# assumption to constrain the input such that all the values to be pushed into the fifo must be positive
+assume {push -> fifo_in[7]==0}
+
+# assertion to check fifo will not overflow (##1 means after 1 cycle, $stable is true if the signal's value is the same as last cycle)
 assert {fifo_full && push && !pop |-> ##1 $stable(fifo_counter)} 
 
-#assertion to check the fifo size cannot exceed 4 (double click the counterexample to see the execution trace)
+# assertion to check the fifo size cannot exceed 4 (double click the counterexample to see the execution trace)
 assert {fifo_counter <= 4}   
 
 #IFT properties of SPV (source to destination)
